b81fd0baa3af6a5554b46563a83113f3cc8bacc2
Murilo <murilpcarneiro@gmail.com>
2025-09-07 20:32:24 -0300

    fix: add import dotenv in connection.ts

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,8 +1,9 @@
 import Database from "better-sqlite3";
+import "dotenv/config";
 import { drizzle } from "drizzle-orm/better-sqlite3";
 import { migrate } from "drizzle-orm/better-sqlite3/migrator";
 
-const sqliteClient = new Database(process.env.DB_FILE_NAME!);
+const sqliteClient = new Database(process.env.DB_FILE_NAME! || "db.sqlite");
 
 export const db = drizzle(sqliteClient);
 
